2301eafee38246ae7af0e1dce4dfab4cf0ff8ab3
Simrat Singh <simrat.khokhar@gmail.com>
2020-10-31 22:11:37 +0530

    removed unused variables and added getter-setter for port and address

--- server_thread.py
+++ server_thread.py
@@ -14,24 +14,20 @@ class ServerProxyGW(threading.Thread):
         super().__init__()
         self.event_start = event_start
         self.event_stop = event_stop
-
-        self.max_connection = 5
-        self.buf_size = 8192
-        self.port = 8282
-        self.status = False
+        self._port = 8282
+        self._addr = '0.0.0.0'
 
     def run(self):
         sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        sock_server.bind(('', self.port))
-        sock_server.listen(self.max_connection)
+        sock_server.bind((self._addr, self._port))
+        sock_server.listen(5)
         sock_server.setblocking(False)
-        printMsg("server started successfully on port {}".format(self.port), on_newline=True)
+        printMsg("server started on {}:{}".format(self._addr, self._port), on_newline=True)
 
         # send 'up' status to main thread
         self.event_start.set()
-        self.status = True
 
         id = 0
         inputs = []
@@ -55,6 +51,16 @@ class ServerProxyGW(threading.Thread):
         else:
             if (sock_server):
                 sock_server.close()
-            self.status = False
             print("[=] server socket closed")
+    
+    @property
+    def port(self): return self._port
 
+    @port.setter
+    def port(self, p): self._port = p
+
+    @property
+    def addr(self): return self._addr
+
+    @addr.setter
+    def addr(self, a): self._addr = a
